306f4ebad7970df93e01dd6b5521825e12c71fb2
m4iyama <myam0118@gmail.com>
2018-10-28 14:40:14 +0900

    add redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,20 @@
+import { createStore, compose, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import { reduxFirestore } from 'redux-firestore'
 import { reactReduxFirebase } from 'react-redux-firebase'
-import { createStore, compose } from 'redux'
 import rootReducer, { initialState } from './reducers'
 import firebaseConfig from '../firebaseConfig'
 
 firebase.initializeApp(firebaseConfig)
 firebase.firestore().settings({ timestampsInSnapshots: true })
+
+const middlewares = []
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger)
+}
 
 const enhancers = [
   reduxFirestore(firebase),
@@ -16,6 +22,7 @@ const enhancers = [
     userProfile: 'users',
     useFirestoreForProfile: true,
   }),
+  applyMiddleware(...middlewares),
 ]
 
 const composedEnhancers = compose(...enhancers)
